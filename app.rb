33f02425af59d024e947b06c7fcf6259c5663b9a
yysaki <yysaki@mail.yysaki.com>
2019-01-31 22:34:32 +0900

    sinatra/reloader tutorial

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
-  "hello world"
+  "hello world again"
+end
+
+get '/hello' do
+  "hello 1000!"
+end
+
+get '/hello' do
+  "hello 2"
 end
