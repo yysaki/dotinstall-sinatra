4d626bca8a4c2a86f822fc50bcef4a70f7918421
yysaki <yysaki@mail.yysaki.com>
2019-01-31 22:42:30 +0900

    wildcard, and regexp

--- app.rb
+++ app.rb
@@ -2,21 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "hello world again"
+  "hello world"
 end
 
-# get '/hello/:name' do
-#   "hello #{params[:name]}"
-# end
-
-# get '/hello/:name' do |name|
-#   "hello #{name}"
-# end
-
-# get '/hello/:fname/:lname' do |f, l|
+# get '/hello/*/*' do |f, l|
 #  "hello #{f} #{l}"
 # end
 
-get '/hello/:fname/?:lname?' do |f, l|
- "hello #{f} #{l}"
+# get '/hello/*/*' do |f, l|
+#  "hello  #{params[:splat][0] }#{params[:splat][1]}"
+# end
+
+get %r{/users/([0-9]*)} do
+ "user id =  #{params[:captures][0]}"
 end
