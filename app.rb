6ad58c7d8b812c6c5a3bcef48856875631dcb6ee
yysaki <yysaki@mail.yysaki.com>
2019-02-01 23:01:06 +0900

    erb

--- app.rb
+++ app.rb
@@ -2,17 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "hello world"
+  erb :index
 end
 
-# get '/hello/*/*' do |f, l|
-#  "hello #{f} #{l}"
-# end
-
-# get '/hello/*/*' do |f, l|
-#  "hello  #{params[:splat][0] }#{params[:splat][1]}"
-# end
-
-get %r{/users/([0-9]*)} do
- "user id =  #{params[:captures][0]}"
+get '/about' do
+  erb :about
 end
