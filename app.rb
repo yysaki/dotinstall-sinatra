9fa0185c9596a8f7546bcdaded16c8d096b62e7e
yysaki <yysaki@mail.yysaki.com>
2019-02-01 23:18:52 +0900

    ActiveRecord

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: './bbs.db'
+)
+
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
   @title = "My BBS"
+  @comments = Comment.all
   erb :index
 end
